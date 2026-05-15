6fd056482d6d60339a5f724ece884d9b58babb93
VadymV <Nikkonofun@gmail.com>
2026-05-15 18:02:13 +0200

    Make fake network layer

--- Assets/Scripts/Server/ServerModel/ServerMain.cs
+++ Assets/Scripts/Server/ServerModel/ServerMain.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using Server.ServerModel.Data;
 using Server.ServerModel.Logic;
 using Shared.SharedModel.Data;
@@ -8,17 +10,41 @@ using Shared.SharedModel.Dto.MakeTurn;
 
 namespace Server.ServerModel
 {
-  // TODO: make asynchronous
   public static class ServerMain
   {
+    // Server simplified. No segments, no multiple matches, etc...
     private static ServerMatchData _matchData;
     private static MatchProcessor _matchProcessor;
-    
-    public static LaunchMatchResponseDto LaunchMatch(LaunchMatchRequestDto request)
-    {
-      _matchData = new MatchInitializer().GenerateMatchData();
-      _matchProcessor = new MatchProcessor();
+    private static Guid? _startMatchRequestId;
+    private static Dictionary<Guid, MakeTurnResponseDto> _history;
 
+    private static readonly Dictionary<string, Func<string, string>> _nameToMethod = new()
+    {
+      { "LaunchMatch", LaunchMatch },
+      { "MakeTurn", MakeTurn },
+    };
+
+    public static string CallMethod(string methodName, string requestJson)
+    {
+      if (!_nameToMethod.TryGetValue(methodName, out var method))
+        return null;  // Simplification. Should return 400 normally
+      
+      return method(requestJson);
+    }
+    
+    private static string LaunchMatch(string requestJson)
+    {
+      var request = JsonConvert.DeserializeObject<LaunchMatchRequestDto>(requestJson);
+      if (_startMatchRequestId != request.RequestId)
+      {
+        _startMatchRequestId = request.RequestId;
+
+        _matchData = new MatchInitializer().GenerateMatchData();
+        _matchProcessor = new MatchProcessor();
+
+        _history = new Dictionary<Guid, MakeTurnResponseDto>();
+      }
+      
       var response = new LaunchMatchResponseDto
       {
         TurnEvents = new Queue<TurnEventData>()
@@ -27,12 +53,19 @@ namespace Server.ServerModel
       for (var i = 0; i < _matchData.Players.Length; i++)
         response.TurnEvents.Enqueue(TurnEventDataHelper.Create(TurnEventKind.GetCardsFromInitialDeck, i, _matchData));
       
-      return response;
+      return JsonConvert.SerializeObject(response);
     }
 
-    public static MakeTurnResponseDto MakeTurn(MakeTurnRequestDto request)
+    private static string MakeTurn(string requestJson)
     {
-      return _matchProcessor.ProcessTurn(_matchData);
+      var request = JsonConvert.DeserializeObject<MakeTurnRequestDto>(requestJson);
+      if (_history.TryGetValue(request.RequestId, out var responseOfId))
+        return JsonConvert.SerializeObject(responseOfId);
+
+      var response = _matchProcessor.ProcessTurn(_matchData);
+      _history.Add(request.RequestId, response);
+      
+      return JsonConvert.SerializeObject(response);
     }
   }
 }
---
+++ Assets/Scripts/Shared/ClientServerFakeLayer/FakeNetwork.cs
@@ -0,0 +1,15 @@
+﻿using Cysharp.Threading.Tasks;
+using Server.ServerModel;
+
+namespace Shared.ClientServerFakeLayer
+{
+  public static class FakeNetwork
+  {
+    public static async UniTask<string> CallServerMethod(string methodName, string requestJson)
+    {
+      // TODO: make delays configurable and request failurable
+      await UniTask.Delay(200);
+      return ServerMain.CallMethod(methodName, requestJson);
+    }
+  }
+}
